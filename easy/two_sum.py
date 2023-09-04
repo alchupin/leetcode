df14fcbf5e76b642a7f1839e620ac759bb162126
chupin <a.chupin@dm-solutions.ru>
2023-09-04 14:49:27 +0300

    Fix

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,16 +1,13 @@
 # https://leetcode.com/problems/two-sum/description/
 
 def two_sum(nums, target):
-    if len(nums) == 2:
-        return [0, 1]
-    
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
                 index_1 = nums.index(nums[i])
                 index_2 = nums.index(nums[j], index_1+1)
                 return [index_1, index_2]
-    
+
 
 # leetcode    
 def two_sum(nums, target):      
