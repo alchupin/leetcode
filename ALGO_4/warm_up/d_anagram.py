86314aa790eb0784a5dd2585a2ec9c5c8686aefb
Aleksey Chupin <alchupin@gmail.com>
2023-10-31 01:44:21 +0300

    Update

---
+++ ALGO_4/warm_up/d_anagram.py
@@ -0,0 +1,21 @@
+s1 = input()
+s2 = input()
+d1 = {}
+d2 = {}
+if not len(s1) == len(s2):
+    print('NO')
+for ch in s1:
+    if ch in d1:
+        d1[ch] += 1
+    else:
+        d1[ch] = 0
+for ch in s2:
+    if ch in d2:
+        d2[ch] += 1
+    else:
+        d2[ch] = 0
+if d1 == d2:
+    print('YES')
+else:
+    print('NO')
+
